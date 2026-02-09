aa28df329512dd3c5efe6beb3e562b2ddf8312b4
saravanan-mcw <saravanan.selvaraj@multicorewareinc.com>
2026-02-09 13:43:46 +0530

    Update : clang_rt path in build_wheels.sh file

--- build_tools/wheels/build_wheels.sh
+++ build_tools/wheels/build_wheels.sh
@@ -61,6 +61,9 @@ if [[ "$RUNNER_OS" == "Windows" && "$CIBW_BUILD" == *"win_arm64"* ]]; then
 
     export CFLAGS="-O3 -funroll-loops"
     export CXXFLAGS="-O3 -funroll-loops"
+
+    CLANG_RT="C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/Llvm/ARM64/lib/clang/19/lib/windows"
+    export LDFLAGS="$LDFLAGS -L\"$CLANG_RT\" -lclang_rt.builtins-aarch64"
 fi
 
 
